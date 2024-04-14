5e4bd3c9bca161386de6dc20fc493b90503f2ca4
AOSXAP <aosxap@gmail.com>
2024-04-14 18:13:50 +0300

    fixed demo

--- demo.sh
+++ demo.sh
@@ -1,3 +1,18 @@
-mkdir bin
-mkdir imgs/results
+if [[ -d bin ]]; then
+  echo "bin directory exists."
+else
+    mkdir bin
+    echo "created bin directory"
+fi
+
+if [[ -d imgs/results ]]; then
+  echo "imgs/results directory exists."
+else
+    mkdir imgs/results
+    echo "created results directory"
+fi
+
 g++ -Iheaders demo.cpp ./src/*.cpp -o ./bin/main
+chmod +x ./bin/main && ./bin/main
+
+echo "Demo results are stored in ./imgs/results"
